41769e45159de4ca13b13ee4ecc2f1cf7c8e8363
m <mark.alex@web.de>
2018-05-09 21:48:49 +0200

    Multiple scripts runnable at the same time with 300 hits per page

--- Classes/Search.py
+++ Classes/Search.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from Classes.Event import *
 import sys
 from time import sleep
 
-
 class Search:
     BASE_URL = 'https://marvin.uni-marburg.de/qisserver/pages/cm/exa/coursemanagement/basicCourseData.xhtml?_flowId=searchCourseNonStaff-flow&_flowExecutionKey=e1s1'
     unhandled_urls = []
-    SEARCH_RESULTS = 10
+    search_results = 10
     driver = None
 
     def __init__(self, url= BASE_URL):
@@ -16,20 +16,29 @@ class Search:
         self.event_ids = set()
         self.driver_start()
 
+    def driver_start(self):
+        try:
+            self.driver = webdriver.Chrome('./chromedriver_osx')
+        except:
+            self.driver = webdriver.Chrome('./chromedriver_ubuntu')
+
     def read_n_search_pages(self, n):
+        """
+        Reads n pages from search. However we get a warning after about 22 pages. Solution: search in multiple windows for speed.
+        :param n:
+        :return:
+        """
         self.empty_search()
         for i in range(n):
-            self.read_search_page(i*10, self.SEARCH_RESULTS)
+            self.read_search_page(i * self.search_results, self.search_results)
             self.next_search_page()
         self.unhandled_urls_to_file()
 
     def read_search_page_range(self, min, max):
-        self.empty_search()
         for i in range(min):
-            sleep(0.1)
             self.next_search_page()
         for i in range(max- min):
-            self.read_search_page((i+min) * 10, self.SEARCH_RESULTS)
+            self.read_search_page((i+min) * self.search_results, self.search_results)
             self.next_search_page()
         self.unhandled_urls_to_file()
 
@@ -47,41 +56,87 @@ class Search:
             self.driver.back()
 
     def unhandled_urls_to_file(self):
-        pd.Series(self.unhandled_urls).to_csv('unhandled_ids', index=False)
+        pd.Series(self.unhandled_urls).to_csv('Unhandled/unhandled_urls', index=False)
         if self.unhandled_urls != []:
             print("WARNING: {} unhandled event pages. See unhandled_ids.csv".format(len(self.unhandled_urls)))
 
     def next_search_page(self):
-        self.driver.back()
-        self.driver.forward()
         element_id = 'genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2next'
-        element = self.driver.find_element(By.ID, element_id)
-        element.click()
+        self.click_wait(element_id)
 
     def open_event(self, idx):
         element_id = 'genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Table:{}:tableRowAction'.format(idx)
-        element = None
+        self.click_wait(element_id)
+
+    def click_wait(self, element_id):
+        # TODO: Use better waiting methods in the below.
         counter = 0
-        while element == None and counter < 10**3:
+        element = None
+        while element == None and counter < 10 ** 3:
             try:
                 element = self.driver.find_element(By.ID, element_id)
             except:
                 counter += 1
-        if counter == 10**3:
+        if counter >= 10 ** 3:
             raise Exception("Error: Element not found. Timeout")
-        element.click()
+
+        counter = 0
+        while counter < 1000:
+            try:
+                element = self.driver.find_element(By.ID, element_id)
+                element.click()
+                break
+            except:
+                counter += 1
+                pass
+        if counter >= 10 ** 3:
+            raise Exception("Error: Element found, but click timeout")
+
+    def send_wait(self, element_id, keys):
+        # TODO: Use better waiting methods in the below.
+        counter = 0
+        element = None
+        while element == None and counter < 10 ** 3:
+            try:
+                element = self.driver.find_element(By.ID, element_id)
+            except:
+                counter += 1
+        if counter >= 10 ** 3:
+            raise Exception("Error: Element not found. Timeout")
+
+        counter = 0
+        while counter < 1000:
+            try:
+                element = self.driver.find_element(By.ID, element_id)
+                element.send_keys(keys)
+                break
+            except:
+                counter += 1
+                pass
+        if counter >= 10 ** 3:
+            raise Exception("Error: Element found, but send timeout")
+
+    def set_entries_count(self, n):
+        id = 'genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2NumRowsInput'
+        self.send_wait(id, 2 * Keys.BACKSPACE)
+        self.send_wait(id, n)
+        self.send_wait(id, Keys.RETURN)
+        self.search_results = n
+
+        # Wait until button "20" disappears. Pretty bullshit hack, but works
+        ele = self.driver.find_element(By.ID, id)
+        btn_20_id = 'genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2idx20'
+        while ele == self.driver.find_element(By.ID, id):
+            try:
+                sleep(0.01)
+            except:
+                print('element not found')
+                break
 
     def empty_search(self):
         self.start()
         suchen_id = 'genericSearchMask:search'
-        element = self.driver.find_element(By.ID, suchen_id)
-        element.click()
+        self.click_wait(suchen_id)
 
     def start(self):
         self.driver.get(self.BASE_URL)
-
-    def driver_start(self):
-        try:
-            self.driver = webdriver.Chrome('./chromedriver_osx')
-        except:
-            self.driver = webdriver.Chrome('./chromedriver_ubuntu')
--- README.md
+++ README.md
@@ -5,23 +5,17 @@ Python3, https://www.python.org/
 Selenium, pip3 install selenium
 Pandas, pip3 install pandas
 SQLalchemy, pip3 install sqlalchemy
+Jupiter, pip3 install jupyter
 
-Zum öffnen von *.ipynb verwendest du terminal command: jupyter notebook
-(jupyter sollte in deiner Python installation enthalten sein)
-
-Für Ergebnisse in SQL, brauchst du eine Datenbank:
+SQL Datenbank:
 host: localhost
 username: postgres
 password: something
 table: events
 
-
 Create Table Events (Titel varchar(300), bemerkung varchar(200), verantwortlicher varchar(500), raum varchar(400),  wochentag varchar(60), von varchar(60)
 					 , bis varchar(60), rhythmus varchar(60), startdatum varchar(60), enddatum varchar(60), organisationseinheit varchar(300)
 					 , permalink varchar(400));
 
-select * from events;
 
-select distinct titel from events order by titel;
-
-truncate table events;
+Programmstart w. CMD: Jupiter notebook
