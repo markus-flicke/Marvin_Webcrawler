3da96fe71ba35e0897e8f2185eb49285f1fab6de
m <mark.alex@web.de>
2018-05-10 23:35:20 +0200

    added code comments. Ready for review

--- Classes/Event.py
+++ Classes/Event.py
@@ -3,64 +3,89 @@ import re
 from sqlalchemy import create_engine
 
 class Event:
+    # Constant to enable dumping our data to a localhost postgres SQL database
     SQL_CONNECTION_STRING = 'postgresql://postgres:something@localhost:5432/Vorlesungsverzeichnis'
-    RELATIONSSHEMA = ['titel', 'bemerkung', 'verantwortlicher', 'raum', 'wochentag', 'von',
-       'bis', 'rhythmus', 'startdatum', 'enddatum', 'organisationseinheit',
-       'permalink']
 
     def __init__(self, html):
+        """
+        To set central data fields
+        :param html:
+        """
+        # HTML source which is analysed in the below
         self.html = html
+        # Results are collected in this table
         self.df = pd.DataFrame()
 
     def summarise(self):
+        """
+        Analyses HTML source and stores findings in self.df
+        """
+        # Obtains a url to the current Event page
         permalink = self.get_permalink()
         try:
+            # Uses a Regex to obtain the lecturers in a single string
             lecturer = self.get_lecturer()
+            # To find a dict of the general information from the box at the top of page
             grunddaten_df = self.get_Grunddaten()
+            # To produce a pd.DataFrame of the Events that are in the events table
             events_df = self.get_Veranstaltungen()
-            # TODO: Talk about how to incorporate Modules in SQL relations
+            # TODO: Incorporate Modules in SQL relations
         except:
+            # In case of error, raise the permalink (and hope it's not NaN, but it usually isnt)
+            # These Errors are found under Marvin_Webcrawler/Errors/...log
             raise Exception(permalink)
 
+        # Restrict the events Dataframe to the Relation Schema
         try:
             df = events_df[['Wochentag', 'Von', 'Bis', 'Rhythmus', 'Startdatum\n', 'Enddatum', 'Raum', 'Bemerkung']]
         except:
             raise Exception('Unusual relation schema: {}'.format(permalink))
 
+        # Format the column names
         df = df.rename(columns={'Startdatum\n': 'Startdatum'})
         df.columns = df.columns.map(lambda s: s.lower())
-        # TODO: Choose appropriate column names here
+        # Assign columns to the information obtained earlier
         df['verantwortlicher'] = lecturer
         df['permalink'] = permalink
         df['titel'] = grunddaten_df['Titel']
         df['organisationseinheit'] = grunddaten_df['Organisationseinheit']
-        # TODO: Maybe delete "(Verantwortlicher)" in ['Organisationseinheit']
         self.df = df
         return self
 
     def sql_append(self):
-        insert_df = self.df[self.RELATIONSSHEMA]
+        """
+        After every pevent pages has been read successfully its information is appended to the SQL table.
+        :return:
+        """
+        # An engine obect from SQLAlchemy using our connection string (DB, user, pw, table, host)
         engine = create_engine(self.SQL_CONNECTION_STRING)
+        # Append if table has been summarised
         if not self.df.empty:
             self.df.to_sql('events', engine, if_exists='append', index=False)
         else:
             self.summarise()
+            self.df.to_sql('events', engine, if_exists='append', index=False)
 
     def get_permalink(self):
         """
-        Returns permalink from Event View html source
+        Returns url to the Event View from html source
+        If none are found, returns NaN
         """
+        # Using a RegEx to match the code sourrunding our Event url
         permalinks = re.findall('data-page-permalink="true">(.*)<', self.html)
         if permalinks == []:
             return pd.np.nan
+        # Replacing HTML fragments to produce the real URL.
+        # I think I am actually trying to parse HTML here -> may fail if there are other replacements.
         return permalinks[0].replace('&amp;', '&')
 
     def get_lecturer(self):
         """
-        Returns Name of Lecturer(s)
-        TODO: Discuss case of multiple lecturers
+        Returns Name of Lecturer(s) separated by a semicolon ';'
         """
+        # Using a RegEx to match the code sourrunding the Lecturer names
         all_lecturers = re.findall('class="linkTableTree">(.*?)<', self.html)
+        # To put all lecturers into a single Attribute in our SQL relation, they are joined with a semicolon
         res = '; '.join(all_lecturers)
         return res
 
@@ -68,8 +93,9 @@ class Event:
         """
         Returns a dictionary of the Grunddaten pane
         """
+        # Using a RegEx to match the code sourrunding all Grunddaten elements
         raw = re.findall('>(.*?)\n</label><div id=".*?" class="answer">(.*?)\n', self.html)
-        # TODO: Clean Data Properly here
+        # TODO: Clean Data Properly here, Data may contain &amp or other weirdness
         return dict(raw)
 
     def get_Veranstaltungen(self):
@@ -77,9 +103,13 @@ class Event:
         Returns a Pandas DataFrame of the Veranstaltungen pane
         TODO: Type conversions (Everything is String right now), could be datetime
         """
+        # Using purely this id to look for the events table
         veranstaltungen_id = 'showEvent:planelementsOfCurrentTerm:0:termineRauemeFieldset1:plannedDatesTable_:plannedDatesTable_Table'
+        # To find the table using, pandas builtin read_html function according to the table's id.
         raw_df = pd.read_html(self.html, attrs = {'id':veranstaltungen_id})[0]
+        # To remove the repetition of the table header in each table element
         df = raw_df.apply(lambda col: col.apply(lambda val: val[len(col.name):]))
+        # To clean values from raw html format into readable text
         df = df.applymap(self._clean_values)
         return df
 
@@ -93,6 +123,12 @@ class Event:
         return df
 
     def _clean_values(self, val):
+        """
+        Parsing HTMl into readable text.
+        TODO: Use a prebuilt (and complete) function for parsing html
+        :param val:
+        :return:
+        """
         val = val.replace('&amp;', '&')
         val = val.strip()
         return val
--- Classes/Search.py
+++ Classes/Search.py
@@ -3,20 +3,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from Classes.Event import *
 import sys
-from time import sleep
+from time import sleep, time
 
 class Search:
+    # URL to start on the first Search page
     BASE_URL = 'https://marvin.uni-marburg.de/qisserver/pages/cm/exa/coursemanagement/basicCourseData.xhtml?_flowId=searchCourseNonStaff-flow&_flowExecutionKey=e1s1'
+    # Collection of Event pages that cannot be read
     unhandled_urls = []
+    # Current number of results displayed per event page
     search_results = 10
+    # Driver to navigate Websites via simulated user action, using the Selenium API
     driver = None
+    # To keep a set of Identifiers of Events currently visible on the Search page
+    event_ids = set()
 
     def __init__(self, url= BASE_URL):
         self.BASE_URL = url
-        self.event_ids = set()
+        # To initialise the selenium driver
         self.driver_start()
 
     def driver_start(self):
+        """
+        Selenium driver is initialised by trial and error.
+        :return:
+        """
+        # Attempt to run osx, ubuntu chromedrivers
+        # TODO: Add more drivers, e.g. Windows such that e.g. the secretary can run this
         try:
             self.driver = webdriver.Chrome('./chromedriver_osx')
         except:
@@ -24,124 +36,157 @@ class Search:
 
     def read_search_page(self, n = 0):
         """
-        Reads the nth search page, starting from a search view.
+        Expects to start on the search view.
+        Reads the nth search page.
         :param n:
         :return:
         """
+        # Navigate to the nth search page
+        # TODO: Go to nth page directly via number btn at btm of page
         for i in range(n):
             self.next_search_page()
+        # Iterate over all expected event IDs to read out all Event pages.
+        # Event IDs depend on their rank in the search results. (for empy search from 0 to ~4046)
         for i in range(self.search_results * n, self.search_results * (n+1)):
+            # Navigate to the event page
             self.open_event(i)
+            # Obtain the event's html source and initialise the Event handler
             event = Event(self.driver.page_source)
+            # Attempt to gather all information from html source
             try:
                 event.summarise()
+                # Read successfull, add found data to SQL
+                event.sql_append()
             except:
-                self.unhandled_urls.append(str(sys.exc_info()[1])) #Find a way to append the error message
+                # If summarising fails, Unhnadled cases are collected (and written to Marvin_Webcrawler/Unhandled/unhandled.csv)
+                self.unhandled_urls.append(str(sys.exc_info()[1]))
+            finally:
+                # Successful or not, navigate back to the search page via "Zuruück" button
                 self.click_wait('showEvent:backButtonTop')
-                continue
-            event.sql_append()
-            self.click_wait('showEvent:backButtonTop')
-            # self.driver.back() TODO: Driver.back works most of the time, but fails randomly. idk why. Got a warning message from server. driver.back would be 500ms faster
+
+        # For unhandled case classification, write unhandled urls to Marvin_Webcrawler / Unhandled / unhandled.csv
+        self.unhandled_urls_to_file()
+        # TODO: Append to file after smaller buckets, instead of attempting to write everything at the end. Doesnt work if process breaks.
 
     def unhandled_urls_to_file(self):
+        # use pandas to create a csv of unhandled urls
         pd.Series(self.unhandled_urls).to_csv('Unhandled/unhandled_urls', index=False)
+        # Warn the User that some Event pages were not read successfully
         if self.unhandled_urls != []:
             print("WARNING: {} unhandled event pages. See unhandled_ids.csv".format(len(self.unhandled_urls)))
 
     def next_search_page(self):
+        """
+        Navigate to the following search page
+        :return:
+        """
+        # Constant next button id
         element_id = 'genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2next'
-        pn = self.page_number()
+        # remember the current page number to compare and check if next page has loaded
+        pn = self.page_number()[0]
+        # Click next including some time buffer to find the element
         self.click_wait(element_id)
-        while self.page_number() == pn:
+        # Note System time to compare time elapsed since going to the next page.
+        start = time()
+        # Wait loop to see a change in page number. Give up if takes longer than 60sec
+        while self.page_number()[0] == pn and time() - start < 60:
             sleep(0.01)
+        # Page number has changed now, else -> Error
+        assert self.page_number()[0] != pn, "Next page not loaded. Timeout"
 
     def page_number(self):
+        """
+        A tuple of (current_page, max_page)
+        :return:
+        """
         html = self.driver.page_source
-        regex = '<span class="dataScrollerPageText">Seite ([0-9]+) von [0-9]+'
-        return int(re.findall(regex, html)[0])
+        # Check html source via RegEx for (current_page, max_page) [shown at btm of search page]
+        regex = '<span class="dataScrollerPageText">Seite ([0-9]+) von ([0-9]+)'
+        # results need to be converted to integer
+        res = re.findall(regex, html)[0]
+        res = [int(res[0]), int(res[1])]
+        return res
 
     def open_event(self, idx):
+        """
+        Opening Event pages via their ID.
+        Depends only on Event's rank in search.
+        :param idx:
+        :return:
+        """
         element_id = 'genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Table:{}:tableRowAction'.format(idx)
         self.click_wait(element_id)
 
-
-    def click_wait(self, element_id):
-        # TODO: Use better waiting methods in the below.
-        import time
-        start = time.time()
+    def click_wait(self, element_id, keys = None):
+        """
+        Click on an element id after waiting for previous process to finish.
+        :param element_id:
+        :return:
+        """
+        # Note time to give up after some time
+        start = time()
+        # Carry forward the number of trials. To see how often selenium attempted to get the event.
         counter = 0
+        # None Element object to see a change here, means some element has been found
         element = None
+        # Keep track of the last attempt to find the element
         attempt_time = None
-        while element == None and time.time() - start < 15:
+        # While no change in element and before the giveup time...
+        while element == None and time() - start < 60:
             try:
+                # Try to find the element until it is found
                 element = self.driver.find_element(By.ID, element_id)
+                if not keys:
+                    # Once the element has been found click
+                    element.click()
+                else:
+                    # Optionally send keys instead of clicking
+                    # TODO: Think of a more fitting name than 'click_wait'
+                    element.send_keys(keys)
             except:
-                attempt_time = time.time()
+                attempt_time = time()
                 counter += 1
-        if element == None:
-            raise Exception("Element not found. Timeout \nAttempts:{} \nLast Attempt (secs before error): {} \nID: {}".format(counter, time.time() - attempt_time, element_id))
-
-        counter = 0
-        while counter < 1000:
-            try:
-                element = self.driver.find_element(By.ID, element_id)
-                element.click()
-                break
-            except:
-                counter += 1
-                pass
-        if counter >= 10 ** 3:
-            raise Exception("Element found, but click Timeout: {}".format(element_id))
-
-    def send_wait(self, element_id, keys):
-        # TODO: Use better waiting methods in the below.
-        counter = 0
-        element = None
-        while element == None and counter < 10 ** 3:
-            try:
-                element = self.driver.find_element(By.ID, element_id)
-            except:
-                counter += 1
-        if counter >= 10 ** 3:
-            raise Exception("Element not found. Timeout: {}".format(element_id))
-
-        counter = 0
-        while counter < 1000:
-            try:
-                element = self.driver.find_element(By.ID, element_id)
-                element.send_keys(keys)
-                break
-            except:
-                counter += 1
-                pass
-        if counter >= 10 ** 3:
-            raise Exception("Element found, but send Timeout: {}".format(element_id))
+        # The element should have been found. Else -> Error Message
+        assert element != None, "Element not found. Timeout \nAttempts:{} \nLast Attempt (secs before error): {} \nID: {}".format(counter, time() - attempt_time, element_id)
 
     def set_entries_count(self, n):
+        """
+        Set the number of entries on the search page.
+        Takes ridiculously long to load for e.g. 300 elements, but may be worth it to not have to switch search pages too much.
+        :param n:
+        :return:
+        """
+        # Set the Input Box at btm of page to n
         id = 'genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2NumRowsInput'
-        self.send_wait(id, 2 * Keys.BACKSPACE)
-        self.send_wait(id, n)
-        self.send_wait(id, Keys.RETURN)
+        # delete old number (10)
+        self.click_wait(id, 2 * Keys.BACKSPACE)
+        self.click_wait(id, n)
+        self.click_wait(id, Keys.RETURN)
+        # Search results are needed to determine event ids. Update this
         self.search_results = n
 
-        # Wait until button "20" disappears. Pretty bullshit hack, but works
-        time_elapsed = 0
-        wait_per_cycle = 0.01
+        # keep trying for at least 120 sec
+        start = time()
+        # Or until the number of max pages changes
+        max_pages = self.page_number()[1]
+        while max_pages == self.page_number()[1] and time() - start < 120:
+            # slight delay - this may keep CPU less busy. idk ¯\_(ツ)_/¯
+            sleep(0.01)
+        # If miracolously the page number has not changed -> Error
+        assert max_pages != self.page_number()[1], "Entries Count. Timeout"
 
-        btn_20_id = 'genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2idx20'
-        ele = self.driver.find_element(By.ID, btn_20_id)
-        while time_elapsed < 15:
-            try:
-                self.driver.find_element(By.ID, btn_20_id)
-                sleep(wait_per_cycle)
-                time_elapsed += wait_per_cycle
-            except:
-                break
 
     def empty_search(self):
+        """
+        Navigate to a Marvin search with all entries
+        :return:
+        """
+        # Start from the Base url
         self.start()
+        # Click "Suchen"
         suchen_id = 'genericSearchMask:search'
         self.click_wait(suchen_id)
 
     def start(self):
+        # Start out on the base url
         self.driver.get(self.BASE_URL)
