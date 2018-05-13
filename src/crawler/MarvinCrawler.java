5674b9ab82e71729d490599b650f4550c8149747
Jakob Eckstein <Eckstein@students.uni-marburg.de>
2018-05-13 17:06:11 +0200

    PageReader is working now

--- src/crawler/MarvinCrawler.java
+++ src/crawler/MarvinCrawler.java
@@ -14,15 +14,18 @@ public class MarvinCrawler {
      * @see <a href="https://github.com/mozilla/geckodriver">https://github.com/mozilla/geckodriver</a>
      */
     public static final String GECKODRIVER_PATH = "./lib/firefoxdriver/geckodriver";
-    //public static final int ENTRIES_PER_PAGE = 300;
+    public static final int ENTRIES_PER_PAGE = 300;
 
     public static void main(String[] args) {
         //initialise Firefox Automation:
         System.setProperty("webdriver.gecko.driver", GECKODRIVER_PATH);
-        PageReader reader = new PageReader(6);
+        PageReader reader = new PageReader(3);
         reader.startEmptySearch();
-        //reader.pause(2000);
+        reader.setEntriesPerPage(ENTRIES_PER_PAGE);
+        System.out.println(reader.getMaxPage());
         reader.goToPage();
-
+        System.out.println(reader.getCurrentPage());
+        reader.readEvents();
+        reader.printEvents();
     }
 }
--- src/crawler/PageReader.java
+++ src/crawler/PageReader.java
@@ -2,14 +2,15 @@ package crawler;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-public class PageReader extends FirefoxDriver{
+public class PageReader extends HtmlUnitDriver{
     /**
      * The URL of ht e Marvin Search Page
      * @see <a href="https://marvin.uni-marburg.de/qisserver/pages/cm/exa/coursemanagement/basicCourseData.xhtml?_flowId=searchCourseNonStaff-flow&_flowExecutionKey=e1s1">Marvin Searchpage</a>
@@ -21,13 +22,20 @@ public class PageReader extends FirefoxDriver{
     private int pageNumber;
 
     public PageReader(int pageNumber) {
-        super();
+        super(true);
         this.pageNumber = pageNumber;
         this.get(MAIN_MARVIN_URL);
     }
 
     public void readEvents() {
         events = this.findElements(By.className(eventClassName));
+    }
+
+    public void printEvents() {
+        Iterator<WebElement> it = events.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next().getText());
+        }
     }
 
 
@@ -52,6 +60,9 @@ public class PageReader extends FirefoxDriver{
         while(!clicked) {
             try {
                 this.findElement(By.id(aPageLinkID)).click();
+                while(this.getCurrentPage() != pageNumber){
+                    this.pause(100);
+                }
                 clicked = true;
             } catch(ElementClickInterceptedException e) {
                 this.pause(500);
@@ -61,13 +72,36 @@ public class PageReader extends FirefoxDriver{
         }
     }
 
+    public void setEntriesPerPage(int entries) {
+        String inputEntriesPerPageID = "genSearchRes:id3df798d58b4bacd9:id3df798d58b4bacd9Navi2NumRowsInput";
+
+        WebElement inputEntriesPerPage = this.findElement(By.id(inputEntriesPerPageID));
+        inputEntriesPerPage.clear();
+        inputEntriesPerPage.sendKeys("" + entries);
+        inputEntriesPerPage.sendKeys( "\n");
+    }
+
     public void pause(long timeout) {
-        try {
-            this.wait(timeout);
-        } catch(InterruptedException ie) {
-            ie.printStackTrace();
+        synchronized (this) {
+            try {
+                this.wait(timeout); //wait has to be in synchronised block to work
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
         }
     }
 
+    public int getMaxPage() {
+        return Integer.parseInt(this.getPageInfo().split(" ")[3]);
+    }
 
+    public int getCurrentPage() {
+        return Integer.parseInt(this.getPageInfo().split(" ")[1]);
+    }
+
+    private String getPageInfo() {
+        String spanPageTextClass = "dataScrollerPageText";
+
+        return this.findElement(By.className(spanPageTextClass)).getText();
+    }
 }
