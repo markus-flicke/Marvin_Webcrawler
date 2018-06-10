35bd19b392dc46abc3258389cc037c710e902f12
Jakob Eckstein <Eckstein@students.uni-marburg.de>
2018-06-10 16:18:18 +0200

    Add everything

---
+++ src/crawler/MarvinCrawler.java
@@ -0,0 +1,16 @@
+package crawler;
+
+public class MarvinCrawler {
+    /**
+     * The relative Path to the Geckodriver for Firefox automation
+     * @see <a href="https://github.com/mozilla/geckodriver">https://github.com/mozilla/geckodriver</a>
+     */
+    private static final String GECKODRIVER_PATH = "./lib/firefoxdriver/geckodriver";    //For Firefox driver
+    private static final int ENTRIES_PER_PAGE = 5;
+    private static int NUMBER_OF_PAGES = 1; //TODO: If possible use PageReader.getMaxPages() to set NUMBER_OF_PAGES to the correct value.
+
+    public static void main(String[] args) {
+        PageNavigator navigator = new PageNavigator();
+        navigator.test(200, 10);
+    }
+}
